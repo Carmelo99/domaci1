05b971aad2d60ac44afc3b964bd7e71609831b59
Carmelo99 <antonije990099@gmail.com>
2022-01-05 11:23:06 +0100

    js funkcionalnost da mora veliko slovo na pocetku imena i prezimena

--- js/novi_zahtev.js
+++ js/novi_zahtev.js
@@ -6,4 +6,30 @@ function prikaziGrad(gradId1, gradId2, gradId3, element)
 }
 
 
- 
+
+ function validacijaIme () {
+    console.log("Prvo slovo imena mora veliko");
+     const poruka=document.getElementById("imePoruka");
+     poruka.innerHTML="";
+     var x = document.getElementById("ime").value;
+     try {
+         if (!x.match(/^[A-Z]{1}/)) throw "Prvo slovo imena mora biti veliko";
+        else poruka.innerHTML="";
+     } catch(err) {
+         poruka.innerHTML=err;
+     }
+ }
+
+
+ function validacijaPrezime () {
+    console.log("Prvo slovo prezimena mora veliko");
+     const poruka=document.getElementById("prezimePoruka");
+     poruka.innerHTML="";
+     var x = document.getElementById("prezime").value;
+     try {
+         if (!x.match(/^[A-Z]{1}/)) throw "Prvo slovo prezimena mora biti veliko";
+        else poruka.innerHTML="";
+     } catch(err) {
+         poruka.innerHTML=err;
+     }
+ }
